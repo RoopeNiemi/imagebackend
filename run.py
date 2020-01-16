fa288db68b6e0edbc74a7a73e12bd633c5dcad3b
Roope Niemi <roope.niemi@helsinki.fi>
2020-01-16 20:34:48 +0200

    port from env value

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from app import app
-
+import os
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    if 'PORT' in  os.environ:
+        port = os.environ['PORT']
+    else:
+        port=5000
+    app.run(debug=True, host='0.0.0.0', port=port)
